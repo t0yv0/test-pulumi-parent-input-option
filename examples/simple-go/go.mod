fd1fc57ce657e34dc7d022688f05d5b2f9a1d9f7
Anton Tayanovskyy <anton@pulumi.com>
2021-07-01 15:16:32 -0400

    Test ParentInput

--- examples/simple-go/go.mod
+++ examples/simple-go/go.mod
@@ -3,8 +3,9 @@ module hello-go
 go 1.14
 
 require (
-	github.com/pulumi/pulumi/sdk/v3 v3.6.0
+	github.com/pulumi/pulumi-aws/sdk/v4 v4.0.0
 	github.com/pulumi/pulumi-xyz/sdk v0.0.1
+	github.com/pulumi/pulumi/sdk/v3 v3.6.1-0.20210701180729-76754321a532
 )
 
 replace github.com/pulumi/pulumi-xyz/sdk => ../../sdk
--- examples/simple-go/main.go
+++ examples/simple-go/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/pulumi/pulumi-aws/sdk/v4/go/aws/s3"
 	"github.com/pulumi/pulumi-xyz/sdk/go/xyz"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
@@ -10,6 +11,26 @@ func main() {
 		sp, err := xyz.NewStaticPage(ctx, "page", &xyz.StaticPageArgs{
 			IndexContent: pulumi.String("<html><body>Hello</body></html>"),
 		})
+		if err != nil {
+			return err
+		}
+
+		bucketResourceInput := sp.Bucket.ApplyT(func(x *s3.Bucket) pulumi.Resource {
+			return x
+		}).(pulumi.ResourceInput)
+
+		_, err = s3.NewBucketObject(
+			ctx,
+			"go.sum",
+			&s3.BucketObjectArgs{
+				Bucket: sp.Bucket.ApplyT(func(x *s3.Bucket) pulumi.IDOutput {
+					return x.ID()
+				}),
+				Source: pulumi.NewFileAsset("go.sum"),
+			},
+			pulumi.ParentInput(bucketResourceInput),
+		)
+
 		if err != nil {
 			return err
 		}
